ee88daa0f686d80fe1917f8a173d51ff8de740b8
Anna Chernikov <annaleezac@gmail.com>
2020-07-02 07:41:47 -0600

    added init to the ops files

--- ops.c
+++ ops.c
@@ -6,3 +6,12 @@ void hello_world ( )
 	printf("Hello World \n");
 
 }
+
+static void *sqfs_hl_op_init(struct fuse_conn_info *conn
+#if FUSE_USE_VERSION >= 30
+			     ,struct fuse_config *cfg
+#endif
+			     ) {
+	return fuse_get_context()->private_data;
+}
+
--- ops.h
+++ ops.h
@@ -1,6 +1,16 @@
+#include "common.h"
+
+#include "squashfs_fs.h"
+
+
 #ifndef SQFS_OPS_H
 #define SQFS_OPS_H
 
 extern void hello_world ( );
 
+extern static void *sqfs_hl_op_init(struct fuse_conn_info *conn
+#if FUSE_USE_VERSION >= 30
+                             ,struct fuse_config *cfg
+#endif
+                             ) ;
 #endif
