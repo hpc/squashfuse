989c897e52426de3ff8cb5ec9123caee6ee3221c
Anna Chernikov <annaleezac@gmail.com>
2020-07-02 07:44:38 -0600

    ditto

--- ops.c
+++ ops.c
@@ -1,4 +1,16 @@
+#include "squashfuse.h"
+#include "fuseprivate.h"
+#include "stat.h"
+
+#include "nonstd.h"
+
+#include <errno.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 #include "ops.h"
 
 void hello_world ( )
