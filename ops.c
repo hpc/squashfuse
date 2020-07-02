c687b2369aeafd97258c572171c6fd4a50e9a9c9
Anna Chernikov <annaleezac@gmail.com>
2020-07-02 09:37:14 -0600

    ugh

--- ops.c
+++ ops.c
@@ -27,7 +27,7 @@ void *sqfs_hl_op_init(struct fuse_conn_info *conn
 	return fuse_get_context()->private_data;
 }
 
-static void sqfs_hl_op_destroy(void *user_data) {
+void sqfs_hl_op_destroy(void *user_data) {
 	sqfs_hl *hl = (sqfs_hl*)user_data;
 	sqfs_destroy(&hl->fs);
 	free(hl);
