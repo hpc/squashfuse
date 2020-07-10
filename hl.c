0e4a2e2521423137ceb76ff1803e2020e65193ef
Anna Chernikov <annaleezac@gmail.com>
2020-07-10 10:44:59 -0600

    removed operations method usage in hl.c

--- hl.c
+++ hl.c
@@ -41,22 +41,7 @@ int main(int argc, char *argv[]) {
 	};
 
 	struct fuse_operations sqfs_hl_ops;
-	memset(&sqfs_hl_ops, 0, sizeof(sqfs_hl_ops));
-	sqfs_hl_ops.init			= sqfs_hl_op_init;
-	sqfs_hl_ops.destroy		= sqfs_hl_op_destroy;
-	sqfs_hl_ops.getattr		= sqfs_hl_op_getattr;
-	sqfs_hl_ops.opendir		= sqfs_hl_op_opendir;
-	sqfs_hl_ops.releasedir	= sqfs_hl_op_releasedir;
-	sqfs_hl_ops.readdir		= sqfs_hl_op_readdir;
-	sqfs_hl_ops.open		= sqfs_hl_op_open;
-	sqfs_hl_ops.create		= sqfs_hl_op_create;
-	sqfs_hl_ops.release		= sqfs_hl_op_release;
-	sqfs_hl_ops.read		= sqfs_hl_op_read;
-	sqfs_hl_ops.readlink	= sqfs_hl_op_readlink;
-	sqfs_hl_ops.listxattr	= sqfs_hl_op_listxattr;
-	sqfs_hl_ops.getxattr	= sqfs_hl_op_getxattr;
-	sqfs_hl_ops.statfs    = sqfs_hl_op_statfs;
-  
+	get_fuse_ops(&sqfs_hl_ops);
 	args.argc = argc;
 	args.argv = argv;
 	args.allocated = 0;
--- ops.h
+++ ops.h
@@ -10,70 +10,12 @@
 
 #ifndef SQFS_OPS_H
 #define SQFS_OPS_H
-
-enum fuse_fill_dir_flags {
-         FUSE_FILL_DIR_PLUS = (1 << 1),
- };
 typedef struct fuse_operations fuse_operations;
-typedef int (*fuse_fill_dir_t) (void *buf, const char *name,
-                                 const struct stat *stbuf, off_t off);
-
-typedef struct off_t offset;
-typedef struct fuse_file_info *fi;
-typedef struct statvfs *st; 
-
-typedef struct fuse_conn_info *conn;
-extern void hello_world ( );
-
-extern void *sqfs_hl_op_init(struct fuse_conn_info *conn
-#if FUSE_USE_VERSION >= 30
-                             ,struct fuse_config *cfg
-#endif
-                             ) ;
 typedef struct sqfs_hl sqfs_hl;
 struct sqfs_hl {
 	sqfs fs;
 	sqfs_inode root;
 };
-extern void sqfs_hl_op_destroy(void *user_data);
-
-int sqfs_hl_op_getattr(const char *path, struct stat *st
-#if FUSE_USE_VERSION >= 30
-                              , struct fuse_file_info *fi
-#endif
-                              );
-int sqfs_hl_op_opendir(const char *path, struct fuse_file_info *fi);
-
-extern int sqfs_hl_op_releasedir(const char *path,
-                struct fuse_file_info *fi);
-
-extern int sqfs_hl_op_readdir(const char *path, void *buf,
-                fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi
-#if FUSE_USE_VERSION >= 30
-        ,enum fuse_readdir_flags flags
-#endif
-        );
-
-int sqfs_hl_op_open(const char *path, struct fuse_file_info *fi);
-int sqfs_hl_op_create(const char* unused_path, mode_t unused_mode,
-		struct fuse_file_info *unused_fi);
-
-extern int sqfs_hl_op_release(const char *path, struct fuse_file_info *fi);
-
-extern int sqfs_hl_op_read(const char *path, char *buf, size_t size,
-                off_t off, struct fuse_file_info *fi);
-
-extern int sqfs_hl_op_readlink(const char *path, char *buf, size_t size);
-extern int sqfs_hl_op_listxattr(const char *path, char *buf, size_t size);
-int sqfs_hl_op_statfs(const char *path, struct statvfs *st);
-
-int sqfs_hl_op_getxattr(const char *path, const char *name,
-                char *value, size_t size
-#ifdef FUSE_XATTR_POSITION
-                , uint32_t position
-#endif
-                );
-
 
 extern void get_fuse_ops(fuse_operations *sqfs_hl_ops);
 
